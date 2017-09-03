33ac1537644c1cd529efdc254ea7b979a3ae9e0b
Kevin Lee <Kemalee@ucsc.edu>
2017-09-03 13:43:29 -0700

    Finished

--- Main.js
+++ Main.js
@@ -54,25 +54,91 @@ function femalelifts(male,female,type,liftType,weightInputs,repMax){
  }
 
  function maleBenchpress(male,female,type,liftType,weightInputs,repMax){
-   var results = 
+   var results = repMax/weightInputs
+   if (results<=.73) {
+     alert("You are a Beginner")
+   }else if (results>.73 && results<=1.2) {
+     alert("You are a Novice")
+   }else if (results>1.2 && results<=1.50) {
+     alert("You are a Intermediate")
+   }else if (results>1.50 && results <=2) {
+     alert("You are Advanced")
+   }else {
+     alert("You are Elite")
+   }
  }
 
  function maleDeadlift(male,female,type,liftType,weightInputs,repMax){
-
+   var results = repMax/weightInputs
+   if (results<=1.35) {
+     alert("You are a Beginner")
+   }else if (results>1.35 && results<=1.7) {
+     alert("You are a Novice")
+   }else if (results>1.7 && results<=2.2) {
+     alert("You are a Intermediate")
+   }else if (results>2.2 && results <=2.6) {
+     alert("You are Advanced")
+   }else {
+     alert("You are Elite")
+   }
  }
 
  function maleSquats(male,female,type,liftType,weightInputs,repMax){
-
+   var results = repMax/weightInputs
+   if (results<=.83) {
+     alert("You are a Beginner")
+   }else if (results>.83 && results<=1.5) {
+     alert("You are a Novice")
+   }else if (results>1.5 && results<=2) {
+     alert("You are a Intermediate")
+   }else if (results>2 && results <=2.5) {
+     alert("You are Advanced")
+   }else {
+     alert("You are Elite")
+   }
  }
 
  function femaleBenchpress(male,female,type,liftType,weightInputs,repMax){
-
+   var results = repMax/weightInputs
+   if (results<=.28) {
+     alert("You are a Beginner")
+   }else if (results>.28 && results<=.7) {
+     alert("You are a Novice")
+   }else if (results>.7 && results<=1) {
+     alert("You are a Intermediate")
+   }else if (results>1 && results <=1.5) {
+     alert("You are Advanced")
+   }else {
+     alert("You are Elite")
+   }
  }
 
  function femaleDeadlift(male,female,type,liftType,weightInputs,repMax){
-
+   var results = repMax/weightInputs
+   if (results<=.55) {
+     alert("You are a Beginner")
+   }else if (results>.55 && results<=1.2) {
+     alert("You are a Novice")
+   }else if (results>1.2 && results<=1.7) {
+     alert("You are a Intermediate")
+   }else if (results>1.7 && results <=2.3) {
+     alert("You are Advanced")
+   }else {
+     alert("You are Elite")
+   }
  }
 
  function femaleSquats(male,female,type,liftType,weightInputs,repMax){
-
+   var results = repMax/weightInputs
+   if (results<=.45) {
+     alert("You are a Beginner")
+   }else if (results>.45 && results<=1.2) {
+     alert("You are a Novice")
+   }else if (results>1.2 && results<=1.6) {
+     alert("You are a Intermediate")
+   }else if (results>1.6 && results <=2.2) {
+     alert("You are Advanced")
+   }else {
+     alert("You are Elite")
+   }
  }
